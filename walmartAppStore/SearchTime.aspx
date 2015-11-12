666f6000f09d45a3c654b0b1437b5e91bb71a22a
murali2020kris <murali2020kris@gmail.com>
2015-11-12 16:33:14 +0000

    testcommit

---
+++ walmartAppStore/SearchTime.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="SearchTime.aspx.cs" Inherits="SearchTime" MasterPageFile="~/MasterPage.master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+       
+    <asp:TextBox ID="txtint" runat="server"></asp:TextBox>
+    <asp:Label ID="lblstring" runat="server" Text="string"></asp:Label>
+    <asp:Label ID="lbltime1" runat="server" Text="Time1"></asp:Label>
+
+    <br />
+    <asp:TextBox ID="txtstring" runat="server"></asp:TextBox><asp:Label ID="lblinteger" runat="server" Text="integer"></asp:Label>
+     <asp:Label ID="lbltime2" runat="server" Text="Time2"></asp:Label>
+    <br />
+    <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+<br />
+<br />
+    
+   
+</asp:Content>
---
+++ walmartAppStore/SearchTime.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
+using System.Diagnostics;
+
+public partial class SearchTime : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        if (txtint.Text != "")
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            SqlCommand cmd;
+            string sql = null;
+
+            connetionString = WebConfigurationManager.ConnectionStrings["Adventureworks"].ConnectionString;
+            sql = "Select rand_string from fyi_random where rand_integer= "+ txtint.Text;
+
+            cnn = new SqlConnection(connetionString);
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            try
+            {
+                cnn.Open();
+                cmd = new SqlCommand(sql, cnn);
+                string count = cmd.ExecuteScalar().ToString();
+                cmd.Dispose();
+                cnn.Close();
+                watch.Stop();
+                lbltime1.Text = watch.Elapsed.Milliseconds.ToString();
+                watch.Reset();
+                lblstring.Text = count;
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
+     
+        
+        
+        if(txtstring.Text != "")
+        {
+
+            string connetionString = null;
+            SqlConnection cnn;
+            SqlCommand cmd;
+            string sql = null;
+
+            connetionString = WebConfigurationManager.ConnectionStrings["Adventureworks"].ConnectionString;
+            sql = "Select rand_integer from fyi_random where rand_string= \'" + txtstring.Text + "\'";
+
+            cnn = new SqlConnection(connetionString);
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            try
+            {
+                cnn.Open();
+                cmd = new SqlCommand(sql, cnn);
+                Int32 count = Convert.ToInt32(cmd.ExecuteScalar());
+                cmd.Dispose();
+                cnn.Close();
+
+                watch.Stop();
+                lbltime2.Text = watch.Elapsed.Milliseconds.ToString();
+                watch.Reset();
+             
+                lblinteger.Text = count.ToString();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Can not open connection ! ");
+            }
+
+        }
+    }
+}
